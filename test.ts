5619c712f92396cf215ce82e99d573ecbf7f50d0
stanislavmircic <stanislavmircic@gmail.com>
2024-10-11 16:27:27 +0200

    Third party unused extensions removed

--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+spikerbit.startHeartRecording()
+basic.forever(function () {
+    serial.writeNumber(spikerbit.heartRate())
+    serial.writeLine("")
+})
